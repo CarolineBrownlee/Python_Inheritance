11a938631a0685f2152423c9021d570a9a52ee29
Caroline Brownlee <brownleecaroline@gmail.com>
2020-01-21 14:32:12 -0600

    committing changes before starting exercises

---
+++ customColorsAndSound.py
@@ -0,0 +1,4 @@
+# INHERITANCE:
+# 1. Allows one type to include all of the public properties and methods of another type.
+# 2. It reduces duplicated code when many types in a program all have the same properties and methods.
+
